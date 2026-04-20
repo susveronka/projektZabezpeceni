1a8504e3100793e898329a64c83aa9c4300217ae
Šustková Veronika <sustkova_veronika@oauh.cz>
2026-04-20 13:20:13 +0200

    chyba v login/prihlaseni

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,7 @@ $routes->get('/', 'Formular::prihlaseni');
 $routes->get('login/prihlaseni', 'Formular::prihlaseni');
 $routes->post('login/loginFinish', 'Formular::loginFinish');
 
-$routes->group('admin', ['filter' => 'prihlaseni'] , static function ($routes){
+$routes->group('admin', ['filter' => 'login/prihlaseni'] , static function ($routes){
 $routes->get('index', 'Admin::index');
 
 });
