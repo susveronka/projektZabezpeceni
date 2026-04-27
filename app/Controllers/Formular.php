7ce24b7c627849b6332a873efd8f694829a4be59
Šustková Veronika <sustkova_veronika@oauh.cz>
2026-04-27 12:30:29 +0200

    .

--- app/Controllers/Formular.php
+++ app/Controllers/Formular.php
@@ -39,18 +39,18 @@ class Formular extends BaseController
 
     public function loginFinish()
     {
-        $login    = $this->request->getPost('login');
-        $password = $this->request->getPost('password');
+        $login    = $this->request->getPost('login'); // Get the login value from the POST request
+        $password = $this->request->getPost('password'); // Get the password value from the POST request
 
-        $logged = $this->ionAuth->login($login, $password);
-        $alertObject = new \stdClass();
+        $logged = $this->ionAuth->login($login, $password); // porovná login a heslo - odpovídá nebo ne
+        $alertObject = new stdClass();
 
-        if ($logged) {
+        if ($logged) { // pokud prošlo přihlásí a přesměruje na stránku admin/index s hláškou o úspěchu
             $alertObject->text = $this->config->errorMessage['loginSuccess'];
             $alertObject->type = "success";
 
             return redirect()->to('admin/index')->with('alert', $alertObject);
-        } else {
+        } else {  // pokud neprošlo, přesměruje zpět na přihlašovací stránku s hláškou o chybě
             $alertObject->text = $this->config->errorMessage['loginDanger'];
             $alertObject->type = "danger";
              return redirect()->to('login/prihlaseni')->with('alert', $alertObject);
